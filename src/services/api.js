6ae6ab7071abf6f85da24ad85cea4b0aa2c25dd8
willdjm <will_djm@hotmail.com>
2024-04-21 01:07:45 -0300

    add api

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: 'https://api-foodexplorer-oqbw.onrender.com'
+  baseURL: 'https://api-foodexplorer-hmwz.onrender.com'
 });
